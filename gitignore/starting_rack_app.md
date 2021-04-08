1a809b1e9b80247068ff031fc55599736a9e5c8f
Anne <anne1365@outlook.com>
2021-04-08 16:23:01 -0700

    Working through 'Growing Your Own Web Framework With Rack' parts 1-2

---
+++ gitignore/starting_rack_app.md
@@ -0,0 +1,4 @@
+- to start the app use `rackup` command
+- when starting up server in terminal prepend w/ `bundle exec`
+- command to start web app from terminal:
+  `bundle exec rackup config.ru -p 9595`
---
+++ hello_world.rb
@@ -0,0 +1,20 @@
+require_relative 'advice'
+
+class HelloWorld
+  def call(env)
+    case env['REQUEST_PATH']
+    when '/'
+      ['200', {"Content-Type" => 'text/html'}, ["<html><body><h2>Hello world!</h2></body></html>"]]
+    when '/advice'
+      piece_of_advice = Advice.new.generate #random piece of advice
+      ['200', {"Content-Type" => 'text/html'}, ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]]
+    else
+      [
+        '404',
+        {"Content-Type" => 'text/html', "Content-Length" => '48'},
+        ["<html><body><h4>404 Not Found</h4></body></html>"]
+      ] 
+    end
+  end
+end
+
